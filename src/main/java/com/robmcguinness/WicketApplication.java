b6a419596c679797c7c5f3f8c6e19d6b3049f85e
robert mcguinness <robert.mcguinness.iii@gmail.com>
2012-02-06 00:55:33 -0500

    kill jessionid
    Signed-off-by: robert mcguinness <robert.mcguinness.iii@gmail.com>

--- src/main/java/com/robmcguinness/WicketApplication.java
+++ src/main/java/com/robmcguinness/WicketApplication.java
@@ -1,18 +1,23 @@
 package com.robmcguinness;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.devutils.stateless.StatelessChecker;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.filter.JavaScriptFilteredIntoFooterHeaderResponse;
 import org.apache.wicket.markup.html.IHeaderResponseDecorator;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
+import org.apache.wicket.protocol.http.servlet.ServletWebResponse;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
 import org.apache.wicket.resource.loader.ClassStringResourceLoader;
 
 import com.robmcguinness.stateless.utils.Validation;
 
 /**
- * Application object for your web application. If you want to run this
- * application without deploying, run the Start class.
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
  * @see com.StartTest.stateless.Start#main(String[])
  * 
@@ -42,6 +47,25 @@ public class WicketApplication extends WebApplication {
 			}
 		});
 
+	}
+
+	/**
+	 * Don't append <code>jsessionid</code> since state is managed in the url
+	 */
+	@Override
+	protected WebResponse newWebResponse(final WebRequest webRequest, HttpServletResponse httpServletResponse) {
+		return new ServletWebResponse((ServletWebRequest) webRequest, httpServletResponse) {
+
+			@Override
+			public String encodeURL(CharSequence url) {
+				return url.toString();
+			}
+
+			@Override
+			public String encodeRedirectURL(CharSequence url) {
+				return encodeURL(url);
+			}
+		};
 	}
 
 	protected boolean isDevelopment() {
